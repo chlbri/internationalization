1a3cd87d154281fb79a3e80b7b367c042aab0eba
chlbri <bri_lvi@icloud.com>
2022-02-25 15:38:27 +0000

    0.0.12

--- tests/locales/en/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const functions_1 = require("../functions");
-const common_json_1 = __importDefault(require("./common.json"));
-exports.default = (0, functions_1.concat)(common_json_1.default);
--- tests/locales/types.d.ts
+++
@@ -1,6 +0,0 @@
-export declare type Language = 'en' | 'fr';
-export declare type State = 'idle' | 'loading' | 'loaded';
-export declare type File = {
-    absolute: string;
-    direct: string;
-};
